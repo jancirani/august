fb0d625a9b511ca2a6f53fd47a25205c9ff60260
janci <jancilondon@gmail.com>
2018-08-06 21:27:42 +0100

    add to stepdef

---
+++ src/test/java/AddToBasketStepDef.java
@@ -0,0 +1,51 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by peraikumar on 06/08/2018.
+ */
+public class AddToBasketStepDef {
+    @Given("^I am on the home page$")
+    public void i_am_on_the_home_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I search for the \"([^\"]*)\"$")
+    public void i_search_for_the(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the result should be shown$")
+    public void the_result_should_be_shown() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I select the first product$")
+    public void i_select_the_first_product() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should navigate to detailed page$")
+    public void i_should_navigate_to_detailed_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on add to basket$")
+    public void i_click_on_add_to_basket() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the item should be add to the basket$")
+    public void the_item_should_be_add_to_the_basket() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
